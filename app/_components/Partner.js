b896b38e31b3266001c5e465210e287e8629dd03
bertbrian <bertbrian97@gmail.com>
2025-12-31 01:50:03 +0700

    remove svg and add nes Farma

--- app/_components/Partner.js
+++ app/_components/Partner.js
@@ -1,4 +1,5 @@
 import trisaktiImg from "@/public/partnerships/apotek-trisakti.png";
+import nesFarmaImg from "@/public/partnerships/NesFarma.png";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,7 +9,7 @@ export default function Partner() {
       <div className="mt-8 text-center">
         <small className="text-center">Our Official Partners</small>
 
-        <div className="flex justify-center">
+        <div className="flex items-center justify-center gap-2">
           <Link href="https://g.co/kgs/JQrH4wJ">
             <Image
               src={trisaktiImg}
@@ -16,7 +17,17 @@ export default function Partner() {
               width={20}
               height={20}
               sizes="full"
-              className="m-2 mx-auto h-auto w-20 rounded-lg"
+              className="m-2 mx-auto h-auto w-24 rounded-lg"
+            />
+          </Link>
+          <Link href="https://share.google/3YvN0rA4Dgly1Bu0f">
+            <Image
+              src={nesFarmaImg}
+              alt="Apotek Nes Farma"
+              width={30}
+              height={30}
+              sizes="full"
+              className="m-2 mx-auto h-auto w-24 rounded-lg"
             />
           </Link>
         </div>
